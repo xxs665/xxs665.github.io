e97e76be8b4b355f4452b332005ae9025175d4cc
kiddxu(许晓盛) <kiddxu@tencent.com>
2018-05-17 19:53:41 +0800

    update

--- sw.js
+++ sw.js
@@ -10,7 +10,15 @@ self.addEventListener('activate', function(event) {
   console.log("SW activated");
 });
 
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!");
   event.respondWith(new Response("Hello world!"));
+});*/
+
+self.addEventListener('fetch', function(event) {
+	if (event.request.url.match(/\.png$/))
+		return fetch({ ... event.request, url: 'https://mc.qcloudimg.com/static/img/7fb3753303b9e330138367e955440ca5/image.png'})
+	return fetch(event.request);
+  // console.log("Caught a fetch!");
+  // event.respondWith(new Response("Hello world!"));
 });
