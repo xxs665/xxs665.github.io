fd380c5a3cd0327ee17f8adec31930f180b06c2c
old kidd <850477916@qq.com>
2018-05-17 19:35:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
 console.log("SW startup");
-
+const version = 'v0.0.1';
 self.addEventListener('install', function(event) {
   console.log("SW installed");
 });
